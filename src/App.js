8ec7d92832132e8d0fdbc451f697ba89f43a1258
sample <ade.sulaiman@simian.co.id>
2024-04-25 02:17:37 +0700

    firstcontent

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 import './MusicPlayer.css';
-import Vinyl from './Vinyll.gif'; 
+import Vinyl from './Vinyl3-unscreen.gif'; 
 import Sampang from './Sampang.png';
-import Piringans from '../src/Vinyl2.gif';
+import Piringans from '../src/Remove Bg2.png';
 import Sound from '../src/Sound.mpeg';
 
 function MusicPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio] = useState(new Audio(Sound));
+  const [imageSrc, setImageSrc] = useState(Vinyl);
 
   const togglePlay = () => {
     if (isPlaying) {
       audio.pause();
+      setIsPlaying(false);
+      setImageSrc(Vinyl);
     } else {
       audio.play();
+      setIsPlaying(true);
+      setImageSrc(Piringans); // Mengubah gambar saat tombol play ditekan
     }
-    setIsPlaying(!isPlaying);
+    toggleAnimation(); 
+  };
+
+  const handlePause = () => {
+    audio.pause();
+    setIsPlaying(false);
+    setImageSrc(Piringans); // Mengubah gambar saat tombol pause ditekan
+    toggleAnimation(); 
+  };
+
+  const toggleAnimation = () => {
+    const vinylImage = document.getElementById('vinyl-image');
+    if (vinylImage) {
+      vinylImage.classList.toggle('rotate');
+    }
   };
 
   return (
@@ -27,21 +46,20 @@ function MusicPlayer() {
           <header className="App-header">
             <h1>Radio Sampang</h1>
           </header>
-          <div className="radio-player" style={{width:'800px'}}>
+          <div className="radio-player">
             <div className="image-container">
-              <img src={isPlaying ? Vinyl : Piringans} alt="Piringan Hitam" />
+              <img id="vinyl-image" src={imageSrc} alt="Piringan Hitam" className="responsive-image" />
               {!isPlaying ? (
                 <button className="play-button" onClick={togglePlay}>
                   <FontAwesomeIcon icon={faPlay} className="play-icon" />
                 </button>
               ) : (
-                <button className="pause-button" onClick={togglePlay}>
+                <button className="pause-button" onClick={handlePause}>
                   <FontAwesomeIcon icon={faPause} className="pause-icon" />
                 </button>
               )}
             </div>
             <h2 style={{color: "white"}}>Dengarkan sekarang</h2>
-            
           </div>
         </div>
       </div>
