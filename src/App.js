9df66d527643781cd3845dcb38bd6680f5874414
sample <ade.sulaiman@simian.co.id>
2024-04-24 15:01:04 +0700

    firstcontent

--- src/App.js
+++ src/App.js
@@ -1,35 +1,47 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons';
 import './MusicPlayer.css';
 import Vinyl from './Vinyll.gif'; 
 import Sampang from './Sampang.png';
 import Piringans from '../src/Vinyl2.gif';
-import Sound from '../src/Sound.mpeg'
+import Sound from '../src/Sound.mpeg';
+
 function MusicPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [audio] = useState(new Audio(Sound));
 
   const togglePlay = () => {
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
     setIsPlaying(!isPlaying);
   };
 
   return (
     <div className="App">
-      <div className="background-container" style={{backgroundImage: `url(${Sampang})`, backgroundSize: 'cover'}}>
+      <div className="background-container" style={{backgroundImage: `url(${Sampang})`}}>
         <div className="container">
-        <header className="App-header">
-          <h1>Radio Sampang</h1>
-        </header>
-          <div className="radio-player">
+          <header className="App-header">
+            <h1>Radio Sampang</h1>
+          </header>
+          <div className="radio-player" style={{width:'800px'}}>
             <div className="image-container">
-              {isPlaying ? (
-                <img src={Vinyl} alt="Piringan Hitam" className="rotate" />
+              <img src={isPlaying ? Vinyl : Piringans} alt="Piringan Hitam" />
+              {!isPlaying ? (
+                <button className="play-button" onClick={togglePlay}>
+                  <FontAwesomeIcon icon={faPlay} className="play-icon" />
+                </button>
               ) : (
-                <img src={Piringans} alt="Piringan Hitam" />
+                <button className="pause-button" onClick={togglePlay}>
+                  <FontAwesomeIcon icon={faPause} className="pause-icon" />
+                </button>
               )}
             </div>
-            <h2 style={{color :"white"}}>Dengarkan sekarang</h2>
-            <audio controls onPlay={() => setIsPlaying(true)} onPause={() => setIsPlaying(false)}>
-              <source src={Sound} type="audio/mpeg" />
-            </audio>
+            <h2 style={{color: "white"}}>Dengarkan sekarang</h2>
+            
           </div>
         </div>
       </div>
